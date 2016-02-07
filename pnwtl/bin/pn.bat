a7a65d6e7cfdd9f13c20dd31404d07360c698e31
stier08 <stier08@gmail.com>
2016-02-07 22:18:14 +0100

    build against WinPython-32bit-3.4.3.7

--- pnwtl/bin/pn.bat
+++ pnwtl/bin/pn.bat
@@ -1,8 +1,8 @@
-set PYTHONHOME=D:\portable\Portable_Python_3.2.5.1\App
+set PYTHONHOME=p:\WinPython-32bit-3.4.3.7\python-3.4.3
 set path=%PYTHONHOME%;%path%;P:\bin\Far30b4131.x86.20141012
 set PyPnPythonExe=%PYTHONHOME%\python.exe
 set PNWinmergecmd=D:\Program Files (x86)\WinMerge\WinMergeU.exe
-set PYTHONPATH=D:\portable\Projects\git\portable\PyLibrary
+set PYTHONPATH=p:\Projects\git\portable\PyLibrary
 
 
 "%PyPnPythonExe%" %~dp0scripts\fixusersettings.py "%~dp0settings\config.xml"
