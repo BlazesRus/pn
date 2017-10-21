1a46e8b331262717ad57050b6b9a14b6bd9b2990
stier08 <stier08@gmail.com>
2017-10-21 19:04:26 +0200

    Microsoft Visual Studio-2017

--- pnwtl/build.cmd
+++ pnwtl/build.cmd
@@ -1,5 +1,5 @@
-set ANT_HOME=D:\portable\ant-1_9_4
-set ANT_CMD=D:\portable\ant-1_9_4\bin\ant
-
+set ANT_HOME=P:\apache-ant-1.10.1
+set ANT_CMD=P:\apache-ant-1.10.1\bin\ant
+cd /d  "%~dp0"
 call "%ANT_CMD%" -f "%~dp0\build.xml"
 pause
