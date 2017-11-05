2471d1699869bf0e1ef47a2ec1430f7a3a73e3a0
stier08 <stier08@gmail.com>
2017-11-05 12:15:45 +0100

    python-3.6.2

--- pnwtl/bin/pn.bat
+++ pnwtl/bin/pn.bat
@@ -1,9 +1,9 @@
-set PYTHONHOME=p:\WinPython-32bit-3.4.4.7Zero\python-3.4.4
+set PYTHONHOME=P:\WinPython-32bit-3.6.2.0Zero\python-3.6.2
 set path=%PYTHONHOME%;%path%;P:\bin\Far30b4131.x86.20141012
 set PyPnPythonExe=%PYTHONHOME%\python.exe
 set PNWinmergecmd=D:\Program Files (x86)\WinMerge\WinMergeU.exe
 set PYTHONPATH=p:\Projects\git\portable\PyLibrary
-
+set PYPN_INIT_PY_PATH=p:\Projects\git\portable\PyLibrary\pypn\init.py
 "%PyPnPythonExe%" -m pyserg.pypn.fixusersettings "%~dp0settings\config.xml"  >> "%~dp0log.log" 2>&1
 
 pn.exe --findexts
