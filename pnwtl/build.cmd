c6dfc97d9335cfe5b0eb1868fb44bda52d8788c8
stier08 <stier08@gmail.com>
2019-08-16 17:51:17 +0200

    update

--- pnwtl/build.cmd
+++ pnwtl/build.cmd
@@ -1,5 +1,4 @@
-set ANT_HOME=P:\apache-ant-1.10.1
-set ANT_CMD=P:\apache-ant-1.10.1\bin\ant
+set ANT_HOME=P:\app\apache-ant-1.10.1
+set ANT_CMD=%ANT_HOME%\bin\ant
 cd /d  "%~dp0"
 call "%ANT_CMD%" -f "%~dp0\build.xml"
-pause
