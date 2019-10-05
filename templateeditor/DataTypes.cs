48dc230dc525f120cf4c7e5b2dc76c81ca75f0d4
Neil McNeight <mcneight+github@gmail.com>
2019-10-04 20:32:35 -0500

    Code Cleanup with VS2019

--- templateeditor/DataTypes.cs
+++ templateeditor/DataTypes.cs
@@ -1,412 +1,418 @@
 using System;
 using System.Collections;
-using System.Xml.Serialization;
 using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace ProjectTemplateEditor
 {
-	public enum NodeType
-	{
-		Set,
-		Group,
-		Category,
-		Value
-	};
+    public enum NodeType
+    {
+        Set,
+        Group,
+        Category,
+        Value
+    };
 
-	public enum SetType
-	{ 
-		[XmlEnum("project")]
-		Project, 
-		[XmlEnum("folder")]
-		Folder, 
-		[XmlEnum("file")]
-		File
-	};
+    public enum SetType
+    {
+        [XmlEnum("project")]
+        Project,
+        [XmlEnum("folder")]
+        Folder,
+        [XmlEnum("file")]
+        File
+    };
 
-	public enum ValueType
-	{
-		FolderPath,
-		FilePath,
-		Text,
-		Boolean,
-		Integer,
-		OptionList
-	};
+    public enum ValueType
+    {
+        FolderPath,
+        FilePath,
+        Text,
+        Boolean,
+        Integer,
+        OptionList
+    };
 
-	public interface IDescribed
-	{
-		string Description
-		{
-			get;
-		}
-	}
+    public interface IDescribed
+    {
+        string Description
+        {
+            get;
+        }
+    }
 
-	public class OptionBase : IDescribed
-	{
-		string _name = "setme";
-		string _desc = "New Option";
-		protected string _default = "";
-		int _helpid = 0;
+    public class OptionBase : IDescribed
+    {
+        private string _name = "setme";
+        private string _desc = "New Option";
+        protected string _default = "";
+        private int _helpid = 0;
 
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
 
-		[XmlAttribute("description")]
-		public string Description
-		{
-			get { return _desc; }
-			set { _desc = value; }
-		}
+        [XmlAttribute("description")]
+        public string Description
+        {
+            get => this._desc;
+            set => this._desc = value;
+        }
 
-		[XmlAttribute("default"), Browsable(false)]
-		public string DefaultValue
-		{
-			get { return _default; }
-			set { _default = value; }
-		}
+        [XmlAttribute("default"), Browsable(false)]
+        public string DefaultValue
+        {
+            get => this._default;
+            set => this._default = value;
+        }
 
-		[XmlAttribute("helpid")]
-		public int HelpID
-		{
-			get { return _helpid; }
-			set { _helpid = value; }
-		}
-	}
+        [XmlAttribute("helpid")]
+        public int HelpID
+        {
+            get => this._helpid;
+            set => this._helpid = value;
+        }
+    }
 
-	[XmlRoot("folderPath")]
-	public class FolderPath : OptionBase
-	{
-	}
+    [XmlRoot("folderPath")]
+    public class FolderPath : OptionBase
+    {
+    }
 
-	[XmlRoot("filePath")]
-	public class FilePath : OptionBase
-	{
-	}
+    [XmlRoot("filePath")]
+    public class FilePath : OptionBase
+    {
+    }
 
-	[XmlRoot("value")]
-	public class OptionListValue
-	{
-		string _desc = "";
-		string _value = "";
+    [XmlRoot("value")]
+    public class OptionListValue
+    {
+        private string _desc = "";
+        private string _value = "";
 
-		[XmlAttribute("description")]
-		public string Description
-		{
-			get { return _desc; }
-			set { _desc = value; }
-		}
+        [XmlAttribute("description")]
+        public string Description
+        {
+            get => this._desc;
+            set => this._desc = value;
+        }
 
-		[XmlAttribute("value")]
-		public string Value
-		{
-			get { return _value; }
-			set { _value = value; }
-		}
+        [XmlAttribute("value")]
+        public string Value
+        {
+            get => this._value;
+            set => this._value = value;
+        }
 
-		public override string ToString()
-		{
-			return Value + " : " + Description;
-		}
+        public override string ToString()
+        {
+            return this.Value + " : " + this.Description;
+        }
 
-		public override int GetHashCode()
-		{
-			return ToString().GetHashCode();
-		}
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
 
-	}
+    }
 
-	public class OptionListDesigner : System.ComponentModel.Design.CollectionEditor
-	{
-		public OptionListDesigner() : base( typeof(ArrayList) )
-		{
-		}
+    public class OptionListDesigner : System.ComponentModel.Design.CollectionEditor
+    {
+        public OptionListDesigner() : base(typeof(ArrayList))
+        {
+        }
 
-		protected override Type CreateCollectionItemType()
-		{
-			return typeof(OptionListValue);
-		}
+        protected override Type CreateCollectionItemType()
+        {
+            return typeof(OptionListValue);
+        }
 
-		protected override System.ComponentModel.Design.CollectionEditor.CollectionForm CreateCollectionForm()
-		{
-			System.ComponentModel.Design.CollectionEditor.CollectionForm form = base.CreateCollectionForm();
-			form.Text = "Option List Editor";
+        protected override System.ComponentModel.Design.CollectionEditor.CollectionForm CreateCollectionForm()
+        {
+            System.ComponentModel.Design.CollectionEditor.CollectionForm form = base.CreateCollectionForm();
+            form.Text = "Option List Editor";
 
-			// Make most of the buttons a bit nicer!
-			foreach(System.Windows.Forms.Control c in form.Controls)
-			{
-				System.Windows.Forms.Button b = c as System.Windows.Forms.Button;
-				if(b != null)
-					if(b.Image == null)
-						b.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			}
+            // Make most of the buttons a bit nicer!
+            foreach (System.Windows.Forms.Control c in form.Controls)
+            {
+                System.Windows.Forms.Button b = c as System.Windows.Forms.Button;
+                if (b != null)
+                {
+                    if (b.Image == null)
+                    {
+                        b.FlatStyle = System.Windows.Forms.FlatStyle.System;
+                    }
+                }
+            }
 
-			return form;
-		}
-
-
-	};
-
-	[XmlRoot("optionlist")]
-	public class OptionList : OptionBase
-	{
-		ArrayList _values = new ArrayList();
-
-		[XmlIgnore]
-		public string Default
-		{
-			get { return DefaultValue; }
-			set { DefaultValue = value; }
-		}
-
-		[XmlElement("value", typeof(OptionListValue)),
-		Editor(typeof(OptionListDesigner), typeof(System.Drawing.Design.UITypeEditor))]
-		public ArrayList Values
-		{
-			get { return _values; }
-			set { _values = value; }
-		}
-	}
-
-	[XmlRoot("option")]
-	public class Option : OptionBase
-	{
-		[XmlIgnore]
-		public bool Default
-		{
-			get { if(_default != string.Empty) return System.Xml.XmlConvert.ToBoolean(_default); return false; }
-			set { _default = System.Xml.XmlConvert.ToString(value); }
-		}
-	}
-
-	[XmlRoot("text")]
-	public class Text : OptionBase
-	{
-		[XmlIgnore]
-		public string Default
-		{
-			get { return _default; }
-			set { _default = value; }
-		}
-	}
-
-	[XmlRoot("int")]
-	public class IntOption : OptionBase
-	{
-		[XmlIgnore]
-		public int Default
-		{
-			get { if(_default != string.Empty) return System.Xml.XmlConvert.ToInt32(_default); return 0; }
-			set { _default = System.Xml.XmlConvert.ToString(value); }
-		}
-	}
-
-	public class OptionFactory
-	{
-		public static OptionBase CreateOption(ValueType vt)
-		{
-			switch(vt)
-			{
-				case ValueType.Integer:
-					return new IntOption();
-				case ValueType.Boolean:
-					return new Option();
-				case ValueType.FilePath:
-					return new FilePath();
-				case ValueType.FolderPath:
-					return new FolderPath();
-				case ValueType.OptionList:
-					return new OptionList();
-				case ValueType.Text:
-					return new Text();
-				default:
-					throw new ApplicationException("Unexpected ValueType value");
-			}
-		}
-
-		private OptionFactory()
-		{
-		}
-	}
-
-	[XmlRoot("category")]
-	public class Category : IDescribed
-	{
-		string _name;
-		string _desc;
-		ArrayList _options = new ArrayList();
-
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
-
-		[XmlAttribute("description")]
-		public string Description
-		{
-			get { return _desc; }
-			set { _desc = value; }
-		}
-
-		[Browsable(false),
-		XmlElement("option", typeof(Option)),
-		XmlElement("optionlist", typeof(OptionList)),
-		XmlElement("int", typeof(IntOption)),
-		XmlElement("text", typeof(Text)),
-		XmlElement("filePath", typeof(FilePath)),
-		XmlElement("folderPath", typeof(FolderPath))]
-		public ArrayList Options
-		{
-			get { return _options; }
-			set { _options = value; }
-		}
-	}
-
-	[XmlRoot("group")]
-	public class Group : IDescribed
-	{
-		string _name;
-		string _desc;
-		ArrayList _cats = new ArrayList();
-
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
-
-		[XmlAttribute("description")]
-		public string Description
-		{
-			get { return _desc; }
-			set { _desc = value; }
-		}
-
-		[Browsable(false),
-		XmlElement("category", typeof(Category))]
-		public ArrayList Categories
-		{
-			get { return _cats; }
-			set { _cats = value; }
-		}
-	}
+            return form;
+        }
 
 
+    };
 
-	[XmlRoot("set")]
-	public class Set
-	{
-		private SetType _type;
-		ArrayList _groups = new ArrayList();
+    [XmlRoot("optionlist")]
+    public class OptionList : OptionBase
+    {
+        private ArrayList _values = new ArrayList();
 
-		[XmlAttribute("type"), Browsable(false)]
-		public SetType Type
-		{
-			set { _type = value; }
-			get { return _type; }
-		}
+        [XmlIgnore]
+        public string Default
+        {
+            get => this.DefaultValue;
+            set => this.DefaultValue = value;
+        }
 
-		[Browsable(false),
-		XmlElement("group", typeof(Group))]
-		public ArrayList Groups
-		{
-			get { return _groups; }
-			set { _groups = value; }
-		}
+        [XmlElement("value", typeof(OptionListValue)),
+        Editor(typeof(OptionListDesigner), typeof(System.Drawing.Design.UITypeEditor))]
+        public ArrayList Values
+        {
+            get => this._values;
+            set => this._values = value;
+        }
+    }
 
-		public Set()
-		{
-		}
+    [XmlRoot("option")]
+    public class Option : OptionBase
+    {
+        [XmlIgnore]
+        public bool Default
+        {
+            get { if (this._default != string.Empty) { return System.Xml.XmlConvert.ToBoolean(this._default); } return false; }
+            set => this._default = System.Xml.XmlConvert.ToString(value);
+        }
+    }
 
-		public Set(SetType type)
-		{
-			_type = type;
-		}
-	}
+    [XmlRoot("text")]
+    public class Text : OptionBase
+    {
+        [XmlIgnore]
+        public string Default
+        {
+            get => this._default;
+            set => this._default = value;
+        }
+    }
 
-	[XmlRoot("projectConfig")]
-	public class ProjectConfig
-	{
-		string _name;
-		string _helpfile;
-		string _id;
-		string _ns;
-		string _icon;
-		ArrayList _sets = new ArrayList();
+    [XmlRoot("int")]
+    public class IntOption : OptionBase
+    {
+        [XmlIgnore]
+        public int Default
+        {
+            get { if (this._default != string.Empty) { return System.Xml.XmlConvert.ToInt32(this._default); } return 0; }
+            set => this._default = System.Xml.XmlConvert.ToString(value);
+        }
+    }
 
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
+    public class OptionFactory
+    {
+        public static OptionBase CreateOption(ValueType vt)
+        {
+            switch (vt)
+            {
+                case ValueType.Integer:
+                    return new IntOption();
+                case ValueType.Boolean:
+                    return new Option();
+                case ValueType.FilePath:
+                    return new FilePath();
+                case ValueType.FolderPath:
+                    return new FolderPath();
+                case ValueType.OptionList:
+                    return new OptionList();
+                case ValueType.Text:
+                    return new Text();
+                default:
+                    throw new ApplicationException("Unexpected ValueType value");
+            }
+        }
 
-		[XmlAttribute("helpfile")]
-		public string HelpFile
-		{
-			get { return _helpfile; }
-			set { _helpfile = value; }
-		}
+        private OptionFactory()
+        {
+        }
+    }
 
-		[XmlAttribute("id")]
-		public string GUID
-		{
-			get { return _id; }
-			set { _id = value; }
-		}
+    [XmlRoot("category")]
+    public class Category : IDescribed
+    {
+        private string _name;
+        private string _desc;
+        private ArrayList _options = new ArrayList();
 
-		[XmlAttribute("icon")]
-		public string Icon
-		{
-			get { return _icon; }
-			set { _icon = value; }
-		}
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
 
-		[XmlAttribute("ns")]
-		public string XmlNameSpace
-		{
-			get { return _ns; }
-			set { _ns = value; }
-		}
+        [XmlAttribute("description")]
+        public string Description
+        {
+            get => this._desc;
+            set => this._desc = value;
+        }
 
-		[Browsable(false),
-		XmlElement("set", typeof(Set))]
-		public ArrayList Sets
-		{
-			get { return _sets; }
-			set { _sets = value; }
-		}
-	}
+        [Browsable(false),
+        XmlElement("option", typeof(Option)),
+        XmlElement("optionlist", typeof(OptionList)),
+        XmlElement("int", typeof(IntOption)),
+        XmlElement("text", typeof(Text)),
+        XmlElement("filePath", typeof(FilePath)),
+        XmlElement("folderPath", typeof(FolderPath))]
+        public ArrayList Options
+        {
+            get => this._options;
+            set => this._options = value;
+        }
+    }
 
-	public class FileLoader
-	{
-		public ProjectConfig Load(string filename)
-		{
-			XmlSerializer xs = new XmlSerializer(typeof(ProjectConfig));
-			// TODO: Exceptions...
-			System.Xml.XmlTextReader xtr = new System.Xml.XmlTextReader(filename);
-			ProjectConfig pc = (ProjectConfig)xs.Deserialize(xtr);
-			xtr.Close();
-			return pc;
-		}
+    [XmlRoot("group")]
+    public class Group : IDescribed
+    {
+        private string _name;
+        private string _desc;
+        private ArrayList _cats = new ArrayList();
 
-		public void Save(string filename, ProjectConfig projConf)
-		{
-			System.Xml.XmlTextWriter xtw = new System.Xml.XmlTextWriter(filename, System.Text.Encoding.UTF8);
-			xtw.Formatting = System.Xml.Formatting.Indented;
-			xtw.Indentation = 1;
-			xtw.IndentChar = '\t';
-			XmlSerializerNamespaces xsn = new XmlSerializerNamespaces();
-			xsn.Add("", "");
-			XmlSerializer xs = new XmlSerializer(typeof(ProjectConfig));
-			xs.Serialize(xtw, projConf, xsn);
-			xtw.Close();
-		}
-	}
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
+
+        [XmlAttribute("description")]
+        public string Description
+        {
+            get => this._desc;
+            set => this._desc = value;
+        }
+
+        [Browsable(false),
+        XmlElement("category", typeof(Category))]
+        public ArrayList Categories
+        {
+            get => this._cats;
+            set => this._cats = value;
+        }
+    }
+
+
+
+    [XmlRoot("set")]
+    public class Set
+    {
+        private SetType _type;
+        private ArrayList _groups = new ArrayList();
+
+        [XmlAttribute("type"), Browsable(false)]
+        public SetType Type
+        {
+            set => this._type = value;
+            get => this._type;
+        }
+
+        [Browsable(false),
+        XmlElement("group", typeof(Group))]
+        public ArrayList Groups
+        {
+            get => this._groups;
+            set => this._groups = value;
+        }
+
+        public Set()
+        {
+        }
+
+        public Set(SetType type)
+        {
+            this._type = type;
+        }
+    }
+
+    [XmlRoot("projectConfig")]
+    public class ProjectConfig
+    {
+        private string _name;
+        private string _helpfile;
+        private string _id;
+        private string _ns;
+        private string _icon;
+        private ArrayList _sets = new ArrayList();
+
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
+
+        [XmlAttribute("helpfile")]
+        public string HelpFile
+        {
+            get => this._helpfile;
+            set => this._helpfile = value;
+        }
+
+        [XmlAttribute("id")]
+        public string GUID
+        {
+            get => this._id;
+            set => this._id = value;
+        }
+
+        [XmlAttribute("icon")]
+        public string Icon
+        {
+            get => this._icon;
+            set => this._icon = value;
+        }
+
+        [XmlAttribute("ns")]
+        public string XmlNameSpace
+        {
+            get => this._ns;
+            set => this._ns = value;
+        }
+
+        [Browsable(false),
+        XmlElement("set", typeof(Set))]
+        public ArrayList Sets
+        {
+            get => this._sets;
+            set => this._sets = value;
+        }
+    }
+
+    public class FileLoader
+    {
+        public ProjectConfig Load(string filename)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(ProjectConfig));
+            // TODO: Exceptions...
+            System.Xml.XmlTextReader xtr = new System.Xml.XmlTextReader(filename);
+            ProjectConfig pc = (ProjectConfig)xs.Deserialize(xtr);
+            xtr.Close();
+            return pc;
+        }
+
+        public void Save(string filename, ProjectConfig projConf)
+        {
+            System.Xml.XmlTextWriter xtw = new System.Xml.XmlTextWriter(filename, System.Text.Encoding.UTF8)
+            {
+                Formatting = System.Xml.Formatting.Indented,
+                Indentation = 1,
+                IndentChar = '\t'
+            };
+            XmlSerializerNamespaces xsn = new XmlSerializerNamespaces();
+            xsn.Add("", "");
+            XmlSerializer xs = new XmlSerializer(typeof(ProjectConfig));
+            xs.Serialize(xtw, projConf, xsn);
+            xtw.Close();
+        }
+    }
 }
--- textclipcreator/Clips.cs
+++ textclipcreator/Clips.cs
@@ -1,126 +1,125 @@
-using System;
 using System.Collections;
 using System.Xml.Serialization;
 
 namespace TextClipCreator
 {
-	/// <summary>
-	/// Collection of Text Clips
-	/// </summary>
-	[XmlRoot("clips")]
-	public class Clips
-	{
-		string _name = "";
-		ArrayList _clips = new ArrayList();
-		bool _modified = false;
+    /// <summary>
+    /// Collection of Text Clips
+    /// </summary>
+    [XmlRoot("clips")]
+    public class Clips
+    {
+        private string _name = "";
+        private ArrayList _clips = new ArrayList();
+        private bool _modified = false;
 
-		public Clips()
-		{
-		}
+        public Clips()
+        {
+        }
 
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
 
-		[XmlElement("clip", Type=typeof(Clip))]
-		public ArrayList TextClips
-		{
-			get { return _clips; }
-			set { _clips = value; }
-		}
+        [XmlElement("clip", Type = typeof(Clip))]
+        public ArrayList TextClips
+        {
+            get => this._clips;
+            set => this._clips = value;
+        }
 
-		public bool Modified
-		{
-			get { return _modified; }
-			set { _modified = value; }
-		}
+        public bool Modified
+        {
+            get => this._modified;
+            set => this._modified = value;
+        }
 
-	}
+    }
 
-	/// <summary>
-	/// One Text Clip
-	/// </summary>
-	public class Clip
-	{
-		string _name = "";
-		string _content = "";
+    /// <summary>
+    /// One Text Clip
+    /// </summary>
+    public class Clip
+    {
+        private string _name = "";
+        private string _content = "";
 
-		public Clip()
-		{
-		}
+        public Clip()
+        {
+        }
 
-		public Clip(string name)
-		{
-			_name = name;
-		}
+        public Clip(string name)
+        {
+            this._name = name;
+        }
 
-		[XmlAttribute("name")]
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get => this._name;
+            set => this._name = value;
+        }
 
-		[XmlText]
-		public string Content
-		{
-			get { return _content; }
-			set { _content = value; }
-		}
+        [XmlText]
+        public string Content
+        {
+            get => this._content;
+            set => this._content = value;
+        }
 
-		public override string ToString()
-		{
-			return _name;
-		}
-	}
+        public override string ToString()
+        {
+            return this._name;
+        }
+    }
 
-	/// <summary>
-	/// Load and Save TextClip files
-	/// </summary>
-	public class ClipStorage
-	{
-		static XmlSerializer s_ser = null;
-		System.Xml.Serialization.XmlSerializerNamespaces s_xsn = null;
-		
-		public ClipStorage()
-		{
-			if(s_ser == null)
-			{
-				s_ser = new XmlSerializer(typeof(Clips));
-				s_xsn = new XmlSerializerNamespaces();
-				s_xsn.Add("", "");
-			}
-		}
+    /// <summary>
+    /// Load and Save TextClip files
+    /// </summary>
+    public class ClipStorage
+    {
+        private static XmlSerializer s_ser = null;
+        private readonly System.Xml.Serialization.XmlSerializerNamespaces s_xsn = null;
 
-		public Clips Load(string filename)
-		{
-			System.Xml.XmlTextReader xtw = new System.Xml.XmlTextReader(filename);
-			try
-			{
-				Clips clips = (Clips)s_ser.Deserialize(xtw);
-				return clips;
-			}
-			finally
-			{
-				xtw.Close();
-			}
-		}
+        public ClipStorage()
+        {
+            if (s_ser == null)
+            {
+                s_ser = new XmlSerializer(typeof(Clips));
+                this.s_xsn = new XmlSerializerNamespaces();
+                this.s_xsn.Add("", "");
+            }
+        }
 
-		public void Save(Clips clips, string filename)
-		{
-			System.Xml.XmlTextWriter xtw = new System.Xml.XmlTextWriter(filename, System.Text.Encoding.UTF8);
-			try
-			{
-				xtw.Formatting = System.Xml.Formatting.Indented;
-				s_ser.Serialize(xtw, clips, s_xsn);
-			}
-			finally
-			{
-				xtw.Close();
-			}
-		}
-	}
+        public Clips Load(string filename)
+        {
+            System.Xml.XmlTextReader xtw = new System.Xml.XmlTextReader(filename);
+            try
+            {
+                Clips clips = (Clips)s_ser.Deserialize(xtw);
+                return clips;
+            }
+            finally
+            {
+                xtw.Close();
+            }
+        }
+
+        public void Save(Clips clips, string filename)
+        {
+            System.Xml.XmlTextWriter xtw = new System.Xml.XmlTextWriter(filename, System.Text.Encoding.UTF8);
+            try
+            {
+                xtw.Formatting = System.Xml.Formatting.Indented;
+                s_ser.Serialize(xtw, clips, this.s_xsn);
+            }
+            finally
+            {
+                xtw.Close();
+            }
+        }
+    }
 }
